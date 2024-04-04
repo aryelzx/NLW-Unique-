83690ea07e676cdb6f75c1fce29286e004167342
Aryel Ramos <aryel.goncalves@livsaude.com.br>
2024-04-04 17:27:25 -0300

    create checkin

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import {
   serializerCompiler,
   validatorCompiler
 } from 'fastify-type-provider-zod'
+import { checkIn } from './routes/checki-in'
 import { createEvent } from './routes/create-event'
 import { getAttendeeBadge } from './routes/get-attendee-badge'
 import { getEvent } from './routes/get-events'
@@ -17,6 +18,7 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(checkIn)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
